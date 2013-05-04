e8be77da7573e696be37657915b7e2a78843e6d0
Lee Jones <scribblethink@gmail.com>
2013-05-04 00:36:49 -0500

    Build out rest of Artifact object

--- lib/pipefitter/artifact.rb
+++ lib/pipefitter/artifact.rb
@@ -1,39 +1,28 @@
+require 'pipefitter/error'
+require 'pipefitter/checksum'
+
 class Pipefitter
   class Artifact
     attr_reader :path, :checksum
+
+    def self.create(source, target)
+      checksum = Checksum.checksum(source)
+      File.open("#{target}.md5", 'w+') { |f| f.write(checksum) }
+      system "tar -C #{File.dirname(source)} -czf #{target} #{File.basename(source)}"
+      new(target, checksum)
+    end
 
     def initialize(path, checksum)
       @path = path
       @checksum = checksum
     end
 
-    def expand_to(target_path)
-      expander = Artifact::Expander.new(self)
-      expander.expand_to(target_path)
+    def expand_to(target)
+      system "tar -C #{target} -xzf #{path}"
     end
 
-    class Expander
-      attr_reader :artifact
-
-      def initialize(artifact)
-        @artifact = artifact
-      end
-
-      def expand_to(target)
-        system "tar -C #{target} -xzf #{artifact.path}"
-      end
-    end
-
-    class Compressor
-      attr_reader :source
-      
-      def initialize(source)
-        @source = source
-      end
-
-      def compress_to(target)
-        system "tar -C #{File.dirname(source)} -czf #{target} #{File.basename(source)}"
-      end
+    def verify_expansion(path)
+      Checksum.checksum(path) == checksum
     end
   end
 end
--- spec/pipefitter/artifact_spec.rb
+++ spec/pipefitter/artifact_spec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 describe Pipefitter::Artifact do
   include Pipefitter::SpecHelper
-  let(:test_root) { '/tmp/pipefitter_tests' }
-  let(:rails_root) { "#{test_root}/stubbed_rails_app" }
 
   before(:each) do
     stub_rails_app(test_root, :destroy_initially => true)
     FileUtils.mkdir(assets_path)
     %w{a b c}.each { |f| FileUtils.touch(File.join(assets_path, "#{f}.css")) }
-    system "tar -C #{rails_root}/public -czf #{artifact_path} assets"
+    system "tar -C #{public_path} -czf #{artifact_path} assets"
   end
 
+  let(:test_root) { '/tmp/pipefitter_tests' }
+  let(:rails_root) { "#{test_root}/stubbed_rails_app" }
   let(:artifact_path) { File.join(test_root, 'artifact.tar.gz') }
   let(:artifact_checksum) { 'abc-123-checksum' }
-  let(:assets_path) { File.join(rails_root, 'public', 'assets') }
+  let(:public_path) { File.join(rails_root, 'public') }
+  let(:assets_path) { File.join(public_path, 'assets') }
   let(:artifact) { Pipefitter::Artifact.new(artifact_path, artifact_checksum) }
 
   subject { Pipefitter::Artifact.new(artifact_path, artifact_checksum) }
@@ -27,41 +28,45 @@ describe Pipefitter::Artifact do
     subject.checksum.should eql(artifact_checksum)
   end
 
-  it 'expands to a path' do
-    expander_stub = stub
-    Pipefitter::Artifact::Expander.should_receive(:new).with(subject).and_return(expander_stub)
-    expander_stub.should_receive(:expand_to).with(File.join(rails_root, 'public'))
-    subject.expand_to(File.join(rails_root, 'public'))
-  end
-
-  describe Pipefitter::Artifact::Expander do
+  describe 'expand_to' do
     before(:each) { FileUtils.rm_rf(assets_path) }
-    subject { Pipefitter::Artifact::Expander.new(artifact) }
 
-    it 'expands archives to a path' do
-      subject.expand_to(File.join(rails_root, 'public'))
+    it 'expands to a path' do
+      subject.expand_to(public_path)
       Dir.glob(File.join(rails_root, 'public', 'assets', '*')).map do |path|
         path.split('/').last
       end.should eql(%w{a.css b.css c.css})
     end
-
-    it 'verifies the checksum of the expanded archive'
-    it 'raises an error if checksum does not match expanded archive'
   end
 
-  describe Pipefitter::Artifact::Compressor do
-    subject { Pipefitter::Artifact::Compressor.new(assets_path) }
-    let(:compressed_artifacts_path) do
-      path = File.join(test_root, 'artifacts')
-      FileUtils.mkdir_p(path)
-      path
+  describe 'verify_expansion' do
+    it 'returns true if checksums match' do
+      subject.expand_to(public_path)
+      Pipefitter::Checksum.should_receive(:checksum).with(assets_path).and_return(artifact_checksum)
+      subject.verify_expansion(assets_path).should be_true
     end
 
-    it 'compresses artifacts' do
-      subject.compress_to(File.join(compressed_artifacts_path, 'result.tar.gz'))
-      Dir.glob("#{compressed_artifacts_path}/*").should eql([File.join(compressed_artifacts_path, 'result.tar.gz')])
+    it 'returns false if checksums do not match' do
+      subject.expand_to(public_path)
+      Pipefitter::Checksum.should_receive(:checksum).with(assets_path).and_return('foo-bar-checksum')
+      subject.verify_expansion(assets_path).should be_false
+    end
+  end
+
+  describe 'create' do
+    subject { Pipefitter::Artifact.create(assets_path, artifact_path) }
+
+    it 'returns an artifact instance' do
+      subject.path.should eql(artifact_path)
+      subject.checksum.should eql(Pipefitter::Checksum.checksum(assets_path))
     end
 
-    it 'stores a checksum of the artifact'
+    it 'stores the artifact' do
+      File.exists?(subject.path).should be_true
+    end
+
+    it 'stores a checksum of the artifact' do
+      File.exists?("#{subject.path}.md5").should be_true
+    end
   end
 end
